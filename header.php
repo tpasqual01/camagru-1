80f548cae845ab6a2be5108af7dcfe4e20ec56ca
dlievre <42@photeam.com>
2017-02-01 19:23:54 +0100

    Update header.php

--- header.php
+++ header.php
@@ -2,7 +2,8 @@
 if ($_SESSION["valide"]=='ok') {
 	$status = 'Login : ';
 	$nom = 'Nom';
-	print '<div id="header"><div class="header_titre"><h1>Camagru</h1></div>'.'<div id="header_user" class="header_user"><p class="header_user">'.$status.$_SESSION["email"].'<br />'.'<a href="logoff.php">Log out</a>'.'<br />'.'<a href="profile.php">Profile</a>'.'</p></div>'.'</div>';
+		$SuperUser = $_SESSION["email"] == 'dominique@lievre.net' ? '<a href="superuser.php">SuperUser</a><br />' : '';
+	print '<div id="header"><div class="header_titre"><h1>Camagru</h1></div>'.'<div id="header_user" class="header_user"><p class="header_user">'.$status.$_SESSION["email"].'<br />'.$SuperUser.'<a href="logoff.php">Log out</a>'.'<br />'.'<a href="profile.php">Profile</a>'.'</p></div>'.'</div>';
 }
 else
 	print '<div id="header"><div class="header_titre"><h1>Camagru</h1></div>'.'<div class="header_user"><p>Not Logged<br /><a href="login.php">Login</a></p></div>'.'</div>';
