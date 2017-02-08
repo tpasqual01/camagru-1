bd8bdf05fa6555ea0bd510c9d2f02c775f7899e7
dlievre <42@photeam.com>
2017-02-08 14:04:53 +0100

    Add files via upload

--- tbl_camagru.sql
+++ tbl_camagru.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Mar 07 Février 2017 à 14:14
+-- Généré le :  Mer 08 Février 2017 à 13:04
 -- Version du serveur :  5.7.11
 -- Version de PHP :  7.0.0
 
@@ -35,6 +35,8 @@ CREATE TABLE `tbl_camagru` (
   `Confirm` int(11) NOT NULL DEFAULT '0',
   `Keyuser` varchar(50) NOT NULL,
   `Cpt_reinit` int(11) NOT NULL DEFAULT '5',
+  `Questionsecrete` int(11) NOT NULL,
+  `Reponsesecrete` varchar(50) NOT NULL,
   `Info` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -42,17 +44,16 @@ CREATE TABLE `tbl_camagru` (
 -- Contenu de la table `tbl_camagru`
 --
 
-INSERT INTO `tbl_camagru` (`Id`, `Nom`, `Prenom`, `email`, `Password`, `Confirm`, `Keyuser`, `Cpt_reinit`, `Info`) VALUES
-(1, 'LIEVRE', 'Dominique', 'dominique@lievre.net', 'test', 1, '5899cbccc2a0f', 5, 'sans'),
-(2, 'PASQUALINI', 'thierry', 'te42pe@gmail.com', 'test', 0, '589887291bb9d', 5, 'info'),
-(3, 'dupond', 'louis', 'dupond@lievre.net', 'test', 0, '', 5, 'free'),
-(4, 'DURAND', 'robert', 'durand@lievre.net', 'test', 0, '', 5, 'free'),
-(5, 'PINGUET', 'Dominique', 'dominique@photeam.com', 'test', 0, '5898ab5e6c3d1', 5, 'info'),
-(6, 'PASQUALI', 'thierry', 'tpasqual@student.42.fr', 'test', 1, 'sdfgsdhf', 5, 'info'),
-(7, 'lievre', 'Dominique', 'portable@photeam.com', 'test', 0, 'sdfgsdhf', 5, 'info'),
-(8, 'lievre', '', 'test', 'ssa', 0, 'sdfgsdhf', 5, 'info'),
-(9, '', '', '', '', 0, '5899a0a61fcb4', 5, 'info'),
-(10, '', '', 'test@lievre.net', '', 0, 'sdfgsdhf', 5, 'info');
+INSERT INTO `tbl_camagru` (`Id`, `Nom`, `Prenom`, `email`, `Password`, `Confirm`, `Keyuser`, `Cpt_reinit`, `Questionsecrete`, `Reponsesecrete`, `Info`) VALUES
+(1, 'LIEVRE', 'Dominique', 'dominique@lievre.net', 'test', 1, '589aea329a2cc', 5, 3, 'vélo', 'sans'),
+(2, 'PASQUALINI', 'thierry', 'te42pe@gmail.com', 'test', 0, '589a082066e91', 5, 3, 'cheval', 'info'),
+(3, 'dupond', 'louis', 'dupond@lievre.net', 'test', 0, '', 5, 0, '', 'free'),
+(4, 'DURAND', 'robert', 'durand@lievre.net', 'test', 0, '', 5, 0, '', 'free'),
+(5, 'PINGUET', 'Dominique', 'dominique@photeam.com', 'test', 0, '589a0199da59a', 5, 0, '', 'info'),
+(6, 'PASQUALI', 'thierry', 'tpasqual@student.42.fr', 'test', 1, 'sdfgsdhf', 5, 0, '', 'info'),
+(7, 'lievre', 'Dominique', 'portable@photeam.com', 'test', 0, 'sdfgsdhf', 5, 0, '', 'info'),
+(10, '', '', 'test@lievre.net', '', 0, 'sdfgsdhf', 5, 0, '', 'info'),
+(12, 'BERTRAND', 'merci', 'merci@adopteunvieux.com', 'test', 0, '5899e67abfeb9', 5, 0, '', 'Info');
 
 --
 -- Index pour les tables exportées
@@ -73,7 +74,7 @@ ALTER TABLE `tbl_camagru`
 -- AUTO_INCREMENT pour la table `tbl_camagru`
 --
 ALTER TABLE `tbl_camagru`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
